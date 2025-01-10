466a510ef6e79b8298a2d729837b08af6f8c56af
GonzaSalda <gonza.salda00@gmail.com>
2025-01-10 20:37:21 -0300

    Update Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,14 +6,14 @@ const Navbar = () => {
       <header className="fixed left-0 right-0 top-0  z-10 bg-white shadow-md">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="relative w-full flex h-16 items-center justify-between">
-            <div className="flex gap-x-2">
+            <div className="md:flex gap-x-2 hidden">
               <p>Logo</p>
               <h1 className="text-[#0D6980] font-semibold">
                 Peluqueria Canina
               </h1>
             </div>
 
-            <div className="flex gap-x-6">
+            <div className="flex mx-auto md:mx-0 gap-x-6">
               <Link className="text-[#0D6980] font-medium" to="/">
                 Inicio
               </Link>
